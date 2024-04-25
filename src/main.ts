758a0b358de2512bb96e0d82fa49925ff4728514
Ismail MRABET <ismail.mrabet1@gmail.com>
2024-04-25 17:43:32 +0200

    edited some files and added new map

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 /// <reference types="@workadventure/iframe-api-typings" />
-import { signInterface } from "./loader/signInterface";
 import { bidingInterface } from "./loader/bidingInterface";
 import leaderboard from "./loader/leaderboard";
+import { masterInterface } from "./loader/masterInterface";
+import { signInterface } from "./loader/signInterface";
 import { voteInterface } from "./loader/voteInterface";
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-import ticketOfficeInterface from "./loader/ticketOfficeInterface";
 import { officeBuyInterface } from "./loader/officeBuyInterface";
+import ticketOfficeInterface from "./loader/ticketOfficeInterface";
 
 console.log("Script started successfully");
 
